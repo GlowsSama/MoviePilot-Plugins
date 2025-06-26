f0b0916072370e7cafb96a76b1097e3a768dfcd5
Glow_ <64869385+GlowsSama@users.noreply.github.com>
2025-06-26 21:42:00 +0800

    Update __init__.py

--- plugins/anistrm100/__init__.py
+++ plugins/anistrm100/__init__.py
@@ -43,8 +43,8 @@ class ANiStrm100(_PluginBase):
     plugin_name = "ANiStrm100"
     plugin_desc = "自动获取当季所有番剧，免去下载，轻松拥有一个番剧媒体库"
     plugin_icon = "https://raw.githubusercontent.com/honue/MoviePilot-Plugins/main/icons/anistrm.png"
-    plugin_version = "2.7.0" # <<< 修改：版本更新
-    plugin_author = "GlowsSama & Gemini"
+    plugin_version = "4.7.0" # <<< 修改：版本更新
+    plugin_author = "GlowsSama"
     author_url = "https://github.com/honue"
     plugin_config_prefix = "anistrm100_"
     plugin_order = 15
